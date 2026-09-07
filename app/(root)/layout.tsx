e4bd7aa1b0f787c9a578cae3b41ee1e5d6125109
Shaun07a <shaunjoseph004@gmail.com>
2026-09-07 22:41:19 +0530

    Added the sidebar tabs and corresponding links

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,16 @@
+import Sidebar from "@/components/Sidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: 'Shaun', lastName: 'Joseph' };
+
+
   return (
     <main className="flex h-screen w-full font-inter">
-        SIDEBAR
+        <Sidebar user={loggedIn}/>
         {children}
     </main>
   );
