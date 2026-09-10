2cf1f1d762fe5fc0b484c851d0b95ecb423b1b67
Shaun07a <shaunjoseph004@gmail.com>
2026-09-10 09:49:31 +0530

    Added a menu icon to the mobile nav bar

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import Image from "next/image"
 
 const MobileNav = ({user}: MobileNavProps) => {
   return (
-    <div>MobileNav</div>
+    <section className="w-full max-w-[264px]">
+      <Sheet>
+        <SheetTrigger>
+          <Image
+            src="/icons/hamburger.svg"
+            width={30}
+            height={30}
+            alt="menu"
+            className="cursor-pointer"
+          />
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Are you absolutely sure?</SheetTitle>
+            <SheetDescription>This action cannot be undone.</SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+    </section>
   )
 }
 
