e8811cde878b8b091dde7eb33846ccede6e7a89f
Shaun07a <shaunjoseph004@gmail.com>
2026-09-10 19:34:13 +0530

    Changes to the Mobile Nav Bar

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -27,19 +27,19 @@ const MobileNav = ({user}: MobileNavProps) => {
             width={30}
             height={30}
             alt="menu"
-            className="cursor-pointer"
+            
           />
         </SheetTrigger>
-        <SheetContent side="left">
-              <Link href="/" className='mb-12 cursor-pointer flex items-center gap-2'>
+        <SheetContent side="left" className="border-none bg-white">
+              <Link href="/" className='cursor-pointer flex items-center gap-1 px-4'>
                   <Image 
                       src="/icons/logo.svg" 
                       width={34}
                       height={34}
                       alt = "Horizon logo"
-                      className='size-[24px] max-xl:size-14'
+                      
                   />
-                  <h1 className='sidebar-logo'>Horizon</h1>
+                  <h1 className='text-26 font-ibm-plex-serif font-bold text-black-1'>Horizon</h1>
               </Link>
 
               {sidebarLinks.map((item)=>{
