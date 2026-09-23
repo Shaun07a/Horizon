2e537910eb8da45a1cd55177602a8307fe82373f
Shaun07a <shaunjoseph004@gmail.com>
2026-09-23 23:37:03 +0530

    Making changes to use appwrite as backend

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -14,6 +14,7 @@ import { authFormSchema } from '@/lib/utils'
 import CustomInput from './CustomInput'
 import { Loader2 } from 'lucide-react'
 import { useRouter } from 'next/navigation'
+import { signIn, signUp } from '@/lib/actions/user.actions'
 
 const AuthForm = ({ type }: { type: string }) => {
   const router = useRouter();
@@ -36,19 +37,19 @@ const AuthForm = ({ type }: { type: string }) => {
 
     try {
       if(type === 'sign-up'){
-        // const newUser = await signUp(data);
+        const newUser = await signUp(data);
 
-        // setUser(newUser);
+        setUser(newUser);
         }
       
 
       if(type === 'sign-in'){
-        // const response = await signIn({
-        //   email: data.email,
-        //   password: data.password,
-        // })
+        const response = await signIn({
+          email: data.email,
+          password: data.password,
+        })
 
-        // if(response) router.push('/')
+        if(response) router.push('/')
       }
     } catch (error) {
       console.log(error);
---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,17 @@
+'use server';
+
+export const signIn = async () => {
+    try {
+        //Mutation / Database / Make fecth
+    } catch(error){
+        console.error('Error', error);
+    }
+}
+
+export const signUp = async (userData: SignUpParams) => {
+    try {
+        //Create a user account
+    } catch(error){
+        console.error('Error', error);
+    }
+}
