01405cdfafb91cf7236ea855b13d352b1b4e49a3
Shaun07a <shaunjoseph004@gmail.com>
2026-09-23 23:28:33 +0530

    Creating routes between the pages

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -13,8 +13,10 @@ import { Form } from "@/components/ui/form"
 import { authFormSchema } from '@/lib/utils'
 import CustomInput from './CustomInput'
 import { Loader2 } from 'lucide-react'
+import { useRouter } from 'next/navigation'
 
 const AuthForm = ({ type }: { type: string }) => {
+  const router = useRouter();
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -29,10 +31,31 @@ const AuthForm = ({ type }: { type: string }) => {
     },
   })
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  const  onSubmit = async (data: z.infer<typeof formSchema>) => {
     setIsLoading(true)
-    console.log(values)
-    setIsLoading(false);
+
+    try {
+      if(type === 'sign-up'){
+        // const newUser = await signUp(data);
+
+        // setUser(newUser);
+        }
+      
+
+      if(type === 'sign-in'){
+        // const response = await signIn({
+        //   email: data.email,
+        //   password: data.password,
+        // })
+
+        // if(response) router.push('/')
+      }
+    } catch (error) {
+      console.log(error);
+    }finally{
+      setIsLoading(false);
+    }
+    
   }
 
   return (
@@ -81,6 +104,7 @@ const AuthForm = ({ type }: { type: string }) => {
                     <CustomInput control={form.control} name='lastName' label='Last Name' placeholder='Enter your last name' />
                   </div>
                   <CustomInput control={form.control} name='address1' label='Address' placeholder='Enter your specific address' />
+                  <CustomInput control={form.control} name='city' label='City' placeholder='Enter your city' />
                   <div className='flex gap-4'>
                     <CustomInput control={form.control} name='state' label='State' placeholder='Example: NY' />
                     <CustomInput control={form.control} name='postalCode' label='Postal Code' placeholder='Example: 11101' />
