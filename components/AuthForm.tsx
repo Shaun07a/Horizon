4a5d923cd2e65636b7a660dfe3346a0b66809da6
Shaun07a <shaunjoseph004@gmail.com>
2026-09-23 22:10:09 +0530

    Changes to the footer

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -92,12 +92,12 @@ const AuthForm = ({ type }: { type: string }) => {
             </form>
           </Form>
           <footer className='flex justify-center gap-1'>
-              <p>
+              <p className='text-14 font-normal text-gray-600'>
                 {type === 'sign-in'
                 ? "Don't have an account?"
                 : "Already have an account?"
                 }</p>
-                <Link href={type === 'sign-in' ? '/sign-up' : 'sign-in'}>
+                <Link href={type === 'sign-in' ? '/sign-up' : 'sign-in'} className='form-link'>
                   {type === 'sign-in' ? 'Sign Up' : 'Sign In'}
                 </Link>
           </footer>
