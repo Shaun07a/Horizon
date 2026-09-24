1faa6a2db3fdbf45ddf5291eaace5b7c9633def4
Shaun07a <shaunjoseph004@gmail.com>
2026-09-25 02:25:30 +0530

    Configuring the sign up page

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,6 +1,7 @@
 'use server';
 
-import { createSessionClient } from "../appwrite";
+import { ID } from "node-appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
 
 export const signIn = async () => {
     try {
@@ -11,8 +12,32 @@ export const signIn = async () => {
 }
 
 export const signUp = async (userData: SignUpParams) => {
+    const {email, password, firstName, lastName} = userData;
+
     try {
-        //Create a user account
+        const { account } = await createAdminClient();
+
+        // Removed the {} and passed as separate positional arguments.
+        // Used backticks (`) for proper string interpolation.
+        const newUserAccount = await account.create(
+            ID.unique(), 
+            email, 
+            password, 
+            `${firstName} ${lastName}`
+        );
+        
+        // ... rest of your code
+        const session = await account.createEmailPasswordSession({
+            email,
+            password
+        });
+
+        cookies().set("my-custom-session", session.secret, {
+            path: "/",
+            httpOnly: true,
+            sameSite: "strict",
+            secure: true,
+        });
     } catch(error){
         console.error('Error', error);
     }
