cad91efbfb28c89d6e350b99372dc8dd8a835d78
Shaun07a <shaunjoseph004@gmail.com>
2026-09-25 02:12:32 +0530

    Setting up the server side

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,5 +1,5 @@
 "use server";
-import { Client, Account } from "node-appwrite";
+import { Client, Account, Databases } from "node-appwrite";
 import { cookies } from "next/headers";
 
 export async function createSessionClient() {
@@ -34,5 +34,8 @@ export async function createAdminClient() {
     get account() {
       return new Account(client);
     },
+    get database() {
+        return new Databases(client)
+    }
   };
 }
