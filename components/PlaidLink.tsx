5c548a49dac9e32e0186722a0181e0746eb801b8
Shaun07a <shaunjoseph004@gmail.com>
2026-09-26 06:34:56 +0530

    Adding the on-click functionality

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -1,52 +1,56 @@
 import { Button } from '@base-ui/react'
-import { StyledString } from 'next/dist/build/swc/types';
 import { useRouter } from 'next/navigation';
 import React, { useCallback, useEffect, useState } from 'react'
-import { PlaidLinkOptions, usePlaidLink } from 'react-plaid-link'
+import { PlaidLinkOnSuccess, PlaidLinkOptions, usePlaidLink } from 'react-plaid-link'
 
 const PlaidLink = ({ user, variant}: PlaidLinkProps) => {
   const router = useRouter();
-  const [token, seToken] = useState('');
+  
+  // FIX 1: Changed seToken to setToken
+  const [token, setToken] = useState('');
 
   useEffect(() => {
     const getLinkToken = async () =>{
         // const data = await createLinkToken(user);
-
         // setToken(data?.linkToken);
     }
-
     getLinkToken();
+  }, [user]);
 
-  }, []);
-
-  const onSuccess = useCallback(async (public_token: StyledString) =>{
+  // FIX 2: Removed `: string` to let TypeScript infer the correct type automatically, and added metadata
+  const onSuccess = useCallback<PlaidLinkOnSuccess>(async (public_token, metadata) =>{
     // await exchangePublicToken({
     //     publicToken: public_token,
     //     user,
     // })
 
     router.push('/');
-  }, [user])
-
+  }, [user, router])
 
   const config: PlaidLinkOptions = {
     token,
     onSuccess
   }
+  
+  // Note: You still need to call usePlaidLink(config) here to actually use the Plaid modal!
+  const { open, ready } = usePlaidLink(config);
+
   return (
     <>
         {variant === 'primary' ? (
             <Button
+                onClick={() => open()}
+                disabled={!ready}
                 className="plaidlink-primary"
             >
                 Connect bank
             </Button>
         ): variant === 'ghost' ? (
-            <Button>
+            <Button onClick={() => open()} disabled={!ready}>
                 Connect bank
             </Button>
         ):(
-            <Button>
+            <Button onClick={() => open()} disabled={!ready}>
                 Connect bank
             </Button>
         )}
